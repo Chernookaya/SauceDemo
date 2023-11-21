caabfe8068f165d1cbe5dd08d973a32cd6028031
antonina.chernookaya <chernookaya.antonina@gmail.com>
2023-11-21 10:14:14 +0300

    PageObject_HomeWork_after_fixed

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -2,15 +2,20 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class CartPage extends BasePage {
 
     private final By CHECKOUT = By.id("checkout");
     private final String DELETE_PRODUCT_PATTERN = "//*[text()='%s']/ancestor::*[contains(@class, 'cart_item_label')]//button";
+    public static final By LIST_PRODUCTS = By.cssSelector(".cart_item");
 
     public CartPage(WebDriver driver) {
         super(driver);
     }
+    /*public void isOpened() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("chekout")));
+    }*/
 
     public void open() {
         driver.get(BASE_URL + "/cart.html");
@@ -23,5 +28,9 @@ public class CartPage extends BasePage {
     public void deleteProduct(String productName) {
         By removeButtonLocator = By.xpath(String.format(DELETE_PRODUCT_PATTERN, productName));
         driver.findElement(removeButtonLocator).click();
+    }
+
+    public int getNumberOfProducts() {
+        return driver.findElements(LIST_PRODUCTS).size();
     }
 }
--- src/test/java/tests/CartPageTest.java
+++ src/test/java/tests/CartPageTest.java
@@ -2,14 +2,18 @@ package tests;
 
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 public class CartPageTest extends BaseTest {
 
     @Test
     public void deleteProduct() {
         loginPage.open();
-        loginPage.login("standard_user", "secret_sauce");;
+        loginPage.login("standard_user", "secret_sauce");
+        ;
         productsPage.addToCart("Sauce Labs Bike Light");
         productsPage.openCart();
         cartPage.deleteProduct("Sauce Labs Bike Light");
+        assertEquals(cartPage.getNumberOfProducts(), 0, "Product not deleted");
     }
 }
